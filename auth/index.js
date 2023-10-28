124498f6c3392d6b0e449e9de447090866a43d41
Hosni Bounechada <hosni.bounechada@gmail.com>
2023-10-28 22:09:38 +0100

    test auth service in isolation

--- auth/index.js
+++ auth/index.js
@@ -11,6 +11,8 @@ const { initializeRabbitMQ } = require("./src/config/rabbitmq");
 })();
 
 function main() {
+  console.log("service Auth starting ...");
+
   const PORT = process.env.PORT || 3000;
 
   initializeRabbitMQ();
